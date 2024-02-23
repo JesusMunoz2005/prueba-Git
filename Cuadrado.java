81064260b6d271d69ab8bc5aa9be89168976a5e2
Jesus <jmunrom1601@g.educaand.es>
2024-02-23 12:45:22 +0100

    Añadido Cuadrado

---
+++ Cuadrado.java
@@ -0,0 +1,17 @@
+public class Cuadrado extends Figura{
+	private double radio;
+	
+	public Cuadrado(double base, double altura){
+	this.radio=radio;
+	this.base=base;
+	this.altura=altura;
+	}
+
+@Override
+public double area(){
+
+/*
+ETC...
+*/
+
+}
